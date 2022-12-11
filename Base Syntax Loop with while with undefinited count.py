14eea97891b8aca46ec627370143885bf10e7807
jalilsatria <wsatria212@gmail.com>
2022-12-11 18:18:45 +0700

    simplify code

--- Base Syntax Loop with while with undefinited count.py
+++ Base Syntax Loop with while with undefinited count.py
@@ -2,25 +2,25 @@
 Program perulangan membaca buku dengan while with undifined count
 """
 
-The_number_of_books = 10
+books_count = 10
 print('Mom said, "Please, read and understand all your books"')
 
-The_count_of_read = 0
-The_number_of_books_read_and_understand = 0
-print(f"The number of books read and understand is {The_number_of_books_read_and_understand} books")
+books_read_count = 0
+read_and_understood_count = 0
+print(f"The number of books read and understand is {read_and_understood_count} books")
 
-while The_count_of_read < The_number_of_books * 2:
-    The_count_of_read = The_count_of_read + 1
-    if The_number_of_books_read_and_understand == 9: # komputer membaca dari 0
-        print(f"The order number {The_number_of_books_read_and_understand + 1} of the books is not understand yet")
+while books_read_count < books_count * 2:
+    books_read_count = books_read_count + 1
+    if read_and_understood_count == 9: # komputer membaca dari 0
+        print(f"The order number {read_and_understood_count + 1} of the books is not understand yet")
         # Manusia membaca dari 1 (maka +1)
     else:
-        The_number_of_books_read_and_understand = The_number_of_books_read_and_understand + 1
-        print(f"The order of reading and understanding the book is {The_number_of_books_read_and_understand}")
+        read_and_understood_count = read_and_understood_count + 1
+        print(f"The order of reading and understanding the book is {read_and_understood_count}")
 
-if The_number_of_books_read_and_understand == The_number_of_books:
+if read_and_understood_count == books_count:
     print('Mom, All books can be read and understand')
 else:
-    print(f'Mom, I just can only read and understand {The_number_of_books_read_and_understand} books')
-print(f'The number of books read and understand is {The_number_of_books_read_and_understand} books')
+    print(f'Mom, I just can only read and understand {read_and_understood_count} books')
+print(f'The number of books read and understand is {read_and_understood_count} books')
 print("finish")
