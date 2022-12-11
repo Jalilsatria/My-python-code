b73202f4821a8c3c84ee94f543eddc153cd63242
jalilsatria <wsatria212@gmail.com>
2022-12-11 18:12:59 +0700

    Base Looping with while

--- Base Syntax Loop with while.py
+++ Base Syntax Loop with while.py
@@ -9,7 +9,7 @@ The_number_of_books_read = 0
 print(f"The number of books read is {The_number_of_books_read} books")
 
 while The_number_of_books_read < The_number_of_books:
-    print(f"The order of reading book is {The_number_of_books_read + 1}")
     The_number_of_books_read = The_number_of_books_read + 1
+    print(f"The order of reading book is {The_number_of_books_read}")
 
 print(f'The_number_of_books_read is {The_number_of_books_read} books')
